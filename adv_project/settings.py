746834e14a88bbe0564ea18791fe504639272acd
cacheUp <bradb26@gmail.com>
2020-03-02 12:38:54 +0300

    more heroku config

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+HEROKU_HOST - config("ALLOWED_HOSTS")
+
+ALLOWED_HOSTS = [HEROKU_HOST, "127.0.0.1:3000"]
 
 
 # Application definition
