e4ec9ff21c7757e090a56bdf56a8bf2114ecfa33
cacheUp <bradb26@gmail.com>
2020-03-02 12:46:18 +0300

    fixed error

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+import django_heroku
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 import os
 from decouple import config
 
@@ -26,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-HEROKU_HOST - config('ALLOWED_HOSTS')
+HEROKU_HOST = config('ALLOWED_HOSTS')
 
 ALLOWED_HOSTS = [HEROKU_HOST, "127.0.0.1:3000"]
 
@@ -117,7 +119,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 
 REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': [
@@ -130,7 +131,7 @@ REST_FRAMEWORK = {
     ),
 }
 
-CORS_ORIGIN_ALLOW_ALL=True
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
@@ -151,5 +152,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-import django_heroku
 django_heroku.settings(locals())
