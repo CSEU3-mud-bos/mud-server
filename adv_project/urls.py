aef9e95b56809cc12052cae175c7716c21c75e5f
cacheUp <bradb26@gmail.com>
2020-03-04 13:48:18 +0300

    auth token

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -3,10 +3,12 @@ from django.urls import path, include
 from rest_framework import routers
 from django.conf.urls import include
 from adventure.api import initialize, move
+from rest_framework.authtoken import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/adv/', include('adventure.urls')),
+    path('api-token-auth/', views.obtain_auth_token)
 ]
