f5460de0c29bea907d41c9796baf2572d13d283b
cacheUp <bradb26@gmail.com>
2020-03-04 13:09:54 +0300

    db

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -14,6 +14,7 @@ import django_heroku
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 import os
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -95,13 +96,10 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
 
+DATABASES['default'] = dj_database_url.config(
+    default=config('DATABASE_URL'), conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
--- adv_project/urls.py
+++ adv_project/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
 from django.conf.urls import include
+from adventure.api import initialize, move
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
